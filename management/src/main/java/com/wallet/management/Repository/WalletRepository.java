5bfb6d7187346c8c15b0a68cf1aae5d5baad7714
Haseeb Khan <77884524+haseeb413@users.noreply.github.com>
2023-11-08 17:00:34 +0500

    Delete management directory

--- management/src/main/java/com/wallet/management/Repository/WalletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wallet.management.Repository;
-
-import com.wallet.management.Entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet,Long> {
-
-
-}
