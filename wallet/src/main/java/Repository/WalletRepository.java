ba3eaf8a25dc6a307fd1c533dec866317e6cdeb3
Haseeb Khan <77884524+haseeb413@users.noreply.github.com>
2023-11-08 16:58:07 +0500

    Delete wallet directory

--- wallet/src/main/java/Repository/WalletRepository.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import Entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet,Long> {
-
-
-}
